409044bf045bb34741d0a7569a4bedcc2e79f770
GautamaShastry <gautamashastry@gmail.com>
2023-01-31 18:56:56 +0530

    Printing numbers from 0 to n

--- test.sh
+++ test.sh
@@ -2,12 +2,21 @@
    # ls
 
 # 2)Check if directory exists or not
-mkdir gautam
-dir=gautam
-if [ -e $dir ]
-then
-echo "Directory Exists"
-else
-echo "Directory does  not exist"
-fi
+# mkdir gautam
+# dir=gautam
+# if [ -e $dir ]
+# then
+# echo "Directory Exists"
+# else
+# echo "Directory does  not exist"
+# fi
 
+#3)Read N value from user and print numbers between 0 to N
+
+read -p "Enter a number: " num 
+i=0
+while [ $i -le $num ]
+do
+echo $i 
+i=`expr $i + 1`
+done
