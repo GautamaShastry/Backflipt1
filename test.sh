1fc9d03d618c6ff1350acd0c234784bda412e271
GautamaShastry <gautamashastry@gmail.com>
2023-01-31 18:52:42 +0530

    Checking if directory exists or not

--- test.sh
+++ test.sh
@@ -1,1 +1,13 @@
-ls
+# 1)List all the files in the directory
+   # ls
+
+# 2)Check if directory exists or not
+mkdir gautam
+dir=gautam
+if [ -e $dir ]
+then
+echo "Directory Exists"
+else
+echo "Directory does  not exist"
+fi
+
